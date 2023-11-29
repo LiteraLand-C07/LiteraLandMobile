3e2e2ea9c7fc7d4b2c4654c81c9da70c0c7a1800
alif <malifalhakim11@gmail.com>
2023-11-29 15:32:43 +0700

    Menambah fitur login

--- lib/Main/models/books.dart
+++ lib/Main/models/books.dart
@@ -4,58 +4,60 @@
 
 import 'dart:convert';
 
-List<Book> bookFromJson(String str) => List<Book>.from(json.decode(str).map((x) => Book.fromJson(x)));
+List<Book> bookFromJson(String str) =>
+    List<Book>.from(json.decode(str).map((x) => Book.fromJson(x)));
 
-String bookToJson(List<Book> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+String bookToJson(List<Book> data) =>
+    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
 
 class Book {
-    Model model;
-    int pk;
-    Fields fields;
+  Model model;
+  int pk;
+  Fields fields;
 
-    Book({
-        required this.model,
-        required this.pk,
-        required this.fields,
-    });
+  Book({
+    required this.model,
+    required this.pk,
+    required this.fields,
+  });
 
-    factory Book.fromJson(Map<String, dynamic> json) => Book(
+  factory Book.fromJson(Map<String, dynamic> json) => Book(
         model: modelValues.map[json["model"]]!,
         pk: json["pk"],
         fields: Fields.fromJson(json["fields"]),
-    );
+      );
 
-    Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => {
         "model": modelValues.reverse[model],
         "pk": pk,
         "fields": fields.toJson(),
-    };
+      };
 }
 
 class Fields {
-    String title;
-    String author;
-    String description;
-    String publisher;
-    int pageCount;
-    String genre;
-    String isbn;
-    Language language;
-    DateTime publishedDate;
+  String title;
+  String author;
+  String description;
+  String publisher;
+  int pageCount;
+  String genre;
+  String isbn;
+  String language;
+  DateTime publishedDate;
 
-    Fields({
-        required this.title,
-        required this.author,
-        required this.description,
-        required this.publisher,
-        required this.pageCount,
-        required this.genre,
-        required this.isbn,
-        required this.language,
-        required this.publishedDate,
-    });
+  Fields({
+    required this.title,
+    required this.author,
+    required this.description,
+    required this.publisher,
+    required this.pageCount,
+    required this.genre,
+    required this.isbn,
+    required this.language,
+    required this.publishedDate,
+  });
 
-    factory Fields.fromJson(Map<String, dynamic> json) => Fields(
+  factory Fields.fromJson(Map<String, dynamic> json) => Fields(
         title: json["title"],
         author: json["author"],
         description: json["description"],
@@ -63,11 +65,11 @@ class Fields {
         pageCount: json["page_count"],
         genre: json["genre"],
         isbn: json["ISBN"],
-        language: languageValues.map[json["language"]]!,
+        language: json["language"],
         publishedDate: DateTime.parse(json["published_date"]),
-    );
+      );
 
-    Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => {
         "title": title,
         "author": author,
         "description": description,
@@ -75,35 +77,26 @@ class Fields {
         "page_count": pageCount,
         "genre": genre,
         "ISBN": isbn,
-        "language": languageValues.reverse[language],
-        "published_date": "${publishedDate.year.toString().padLeft(4, '0')}-${publishedDate.month.toString().padLeft(2, '0')}-${publishedDate.day.toString().padLeft(2, '0')}",
-    };
+        "language": language,
+        "published_date":
+            "${publishedDate.year.toString().padLeft(4, '0')}-${publishedDate.month.toString().padLeft(2, '0')}-${publishedDate.day.toString().padLeft(2, '0')}",
+      };
 }
 
-enum Language {
-    ENGLISH
-}
+// ignore: constant_identifier_names
+enum Model { SHARED_MODELS_BOOK }
 
-final languageValues = EnumValues({
-    "English": Language.ENGLISH
-});
-
-enum Model {
-    SHARED_MODELS_BOOK
-}
-
-final modelValues = EnumValues({
-    "shared_models.book": Model.SHARED_MODELS_BOOK
-});
+final modelValues =
+    EnumValues({"shared_models.book": Model.SHARED_MODELS_BOOK});
 
 class EnumValues<T> {
-    Map<String, T> map;
-    late Map<T, String> reverseMap;
+  Map<String, T> map;
+  late Map<T, String> reverseMap;
 
-    EnumValues(this.map);
+  EnumValues(this.map);
 
-    Map<T, String> get reverse {
-        reverseMap = map.map((k, v) => MapEntry(v, k));
-        return reverseMap;
-    }
+  Map<T, String> get reverse {
+    reverseMap = map.map((k, v) => MapEntry(v, k));
+    return reverseMap;
+  }
 }
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:litera_land_mobile/Main/screens/login.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Provider(
+      create: (_) {
+        CookieRequest request = CookieRequest();
+        return request;
+      },
+      child: MaterialApp(
+          title: 'Flutter App',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
+            useMaterial3: true,
+          ),
+          home: const LoginPage()),
+    );
+  }
+}
