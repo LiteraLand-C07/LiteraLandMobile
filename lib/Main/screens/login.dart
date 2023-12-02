8c42ef3451d131c55781a1e8e44c9b9076682b5b
slmnrl <salma.nurul21@ui.ac.id>
2023-12-02 18:46:10 +0700

    update tampilan booklist

--- lib/Main/screens/login.dart
+++ lib/Main/screens/login.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: library_private_types_in_public_api, use_build_context_synchronously
 
 import 'package:flutter/material.dart';
+import 'package:litera_land_mobile/BookLists/screens/book_lists.dart';
 import 'package:litera_land_mobile/collections/screens/mycollection.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'package:provider/provider.dart';
