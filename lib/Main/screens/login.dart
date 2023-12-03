250d69f0d9d4e6e13bc6d28c1705cd4df2854158
slmnrl <salma.nurul21@ui.ac.id>
2023-12-03 14:29:33 +0700

    update

--- lib/Main/screens/login.dart
+++ lib/Main/screens/login.dart
@@ -1,8 +1,8 @@
 // ignore_for_file: library_private_types_in_public_api, use_build_context_synchronously
 
+
 import 'package:flutter/material.dart';
-import 'package:litera_land_mobile/BookLists/screens/book_lists.dart';
-import 'package:litera_land_mobile/collections/screens/mycollection.dart';
+import 'package:litera_land_mobile/BrowseBooks/screens/browse_books_page.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'package:provider/provider.dart';
 
