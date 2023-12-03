cd1cf36046722244acf75c1d592b7fa0c6b4d1b0
NuChraee <caesarramadhan.cr05@gmail.com>
2023-12-03 21:07:04 +0700

    update new code

--- lib/Discuss/models/review_data.dart
+++ lib/Discuss/models/review_data.dart
@@ -4,27 +4,68 @@
 
 import 'dart:convert';
 
-ReviewBook reviewBookFromJson(String str) => ReviewBook.fromJson(json.decode(str));
+List<ReviewBook> reviewBookFromJson(String str) =>
+    List<ReviewBook>.from(json.decode(str).map((x) => ReviewBook.fromJson(x)));
 
-String reviewBookToJson(ReviewBook data) => json.encode(data.toJson());
+String reviewBookToJson(List<ReviewBook> data) =>
+    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
 
 class ReviewBook {
-    String greeting;
-    List<String> instructions;
+  String model;
+  int pk;
+  Fields fields;
 
-    ReviewBook({
-        required this.greeting,
-        required this.instructions,
-    });
+  ReviewBook({
+    required this.model,
+    required this.pk,
+    required this.fields,
+  });
 
-    factory ReviewBook.fromJson(Map<String, dynamic> json) => ReviewBook(
-        greeting: json["greeting"],
-        instructions: List<String>.from(json["instructions"].map((x) => x)),
-    );
+  factory ReviewBook.fromJson(Map<String, dynamic> json) => ReviewBook(
+        model: json["model"],
+        pk: json["pk"],
+        fields: Fields.fromJson(json["fields"]),
+      );
 
-    Map<String, dynamic> toJson() => {
-        "greeting": greeting,
-        "instructions": List<dynamic>.from(instructions.map((x) => x)),
-    };
+  Map<String, dynamic> toJson() => {
+        "model": model,
+        "pk": pk,
+        "fields": fields.toJson(),
+      };
 }
- 
+
+class Fields {
+  int? user;
+  int book;
+  String review;
+  String reviewerName;
+  DateTime date;
+  int starRating;
+
+  Fields({
+    required this.user,
+    required this.book,
+    required this.review,
+    required this.reviewerName,
+    required this.date,
+    required this.starRating,
+  });
+
+  factory Fields.fromJson(Map<String, dynamic> json) => Fields(
+        user: json["user"],
+        book: json["book"],
+        review: json["review"],
+        reviewerName: json["reviewer_name"],
+        date: DateTime.parse(json["date"]),
+        starRating: json["star_rating"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "user": user,
+        "book": book,
+        "review": review,
+        "reviewer_name": reviewerName,
+        "date": date.toIso8601String(),
+        "star_rating": starRating,
+      };
+}
