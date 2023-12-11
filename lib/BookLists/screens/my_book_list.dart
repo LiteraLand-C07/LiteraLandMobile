2d5199fc938647c261671b9ec65b239e24ad1521
slmnrl <salma.nurul21@ui.ac.id>
2023-12-11 21:06:03 +0700

    udah bisa delete

--- lib/BookLists/screens/my_book_list.dart
+++ lib/BookLists/screens/my_book_list.dart
@@ -19,9 +19,8 @@ class MyBookListsPage extends StatefulWidget {
 class _MyBookListsPageState extends State<MyBookListsPage> {
   Future<List<BookLists>> fetchBookLists() async {
     final request = context.watch<CookieRequest>();
-    final response = await request.get(
-        'https://literaland-c07-tk.pbp.cs.ui.ac.id/rankingBuku/get_my_book_lists/');
-
+    final response = await request
+        .get('http://127.0.0.1:8000/rankingBuku/get_my_book_lists/');
 
     List<BookLists> listItem = [];
     for (var d in response) {
@@ -46,42 +45,40 @@ class _MyBookListsPageState extends State<MyBookListsPage> {
       bottomNavigationBar: const MyBottomNavigationBar(
         selectedIndex: 1,
       ),
-      body: Column(
-        children: [
-
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) =>
-                              const MyBookListsPage()), 
-                    );
-                  },
-                  child: const Text('Your List', style: TextStyle(color: Colors.black)),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    // Handle the second button press
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) =>
-                              const BookListsPage()), 
-                    );
-                  },
-                  child: const Text('Explore Others', style: TextStyle(color: Colors.black)),
-                ),
-              ],
-            ),
+      body: Column(children: [
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const MyBookListsPage()),
+                  );
+                },
+                child: const Text('Your List',
+                    style: TextStyle(color: Colors.black)),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  // Handle the second button press
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const BookListsPage()),
+                  );
+                },
+                child: const Text('Explore Others',
+                    style: TextStyle(color: Colors.black)),
+              ),
+            ],
           ),
-          Expanded(
-            child: FutureBuilder(
+        ),
+        Expanded(
+          child: FutureBuilder(
               future: fetchBookLists(),
               builder: (context, AsyncSnapshot snapshot) {
                 if (snapshot.data == null) {
@@ -92,7 +89,8 @@ class _MyBookListsPageState extends State<MyBookListsPage> {
                       children: [
                         Text(
                           "Tidak ada data produk.",
-                          style: TextStyle(color: Color(0xff59A5D8), fontSize: 20),
+                          style:
+                              TextStyle(color: Color(0xff59A5D8), fontSize: 20),
                         ),
                         SizedBox(height: 8),
                       ],
@@ -110,8 +108,8 @@ class _MyBookListsPageState extends State<MyBookListsPage> {
                                 Navigator.push(
                                   context,
                                   MaterialPageRoute(
-                                    builder: (context) =>
-                                        DetailPage(bookList: snapshot.data[index]),
+                                    builder: (context) => DetailPage(
+                                        bookList: snapshot.data[index]),
                                   ),
                                 );
                               },
@@ -120,21 +118,62 @@ class _MyBookListsPageState extends State<MyBookListsPage> {
                                 elevation: 4,
                                 child: Container(
                                   margin: const EdgeInsets.all(8.0),
-                                  child: BookListsWidget(
-                                      bookList: snapshot.data[index]),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.stretch,
+                                    children: [
+                                      BookListsWidget(
+                                          bookList: snapshot.data[index]),
+                                      Align(
+                                        alignment: Alignment.topRight,
+                                        child: GestureDetector(
+                                          onTap: () async {
+                                            // Get the collectionId to be deleted
+                                            int id = snapshot.data[index].pk;
+
+                                            // Send a delete request to the server
+                                            final request = context.read<CookieRequest>();
+                                            final response = await request.post(
+                                                'http://127.0.0.1:8000/rankingBuku/delete-booklist-flutter/$id/', "");
+
+                                            // Check if the deletion was successful
+                                            if (response['status'] == 'success') {
+                                              // Perform any additional actions after successful deletion
+                                              // For example: Show a success message or update the UI
+                                              // Optionally, you can remove the item from the local list
+                                              setState(() {
+                                                snapshot.data.removeAt(index);
+                                              });
+                                            } else {
+                                              // Handle errors, such as displaying an error message
+                                              print(
+                                                  'Failed to delete item. Status code: ${response.statusCode}');
+                                            }
+                                          },
+                                          child: Container(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: const Text(
+                                              'Delete',
+                                              style: TextStyle(
+                                                color: Colors.red,
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      )
+                                    ],
+                                  ),
                                 ),
-                            )
-                          );
+                              ));
                         },
                       ),
                     );
                   }
                 }
               }),
-          )
-          
-          ]
-        ), 
+        )
+      ]),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           // Handle navigation to the page for adding a new product
