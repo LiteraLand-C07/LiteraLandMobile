6eee0de713492bee32cc460784c97067a34ec108
NuChraee <caesarramadhan.cr05@gmail.com>
2023-12-12 00:56:28 +0700

    progress 90%

--- lib/Discuss/screens/review_list.dart
+++ lib/Discuss/screens/review_list.dart
@@ -1,11 +1,10 @@
 import 'dart:convert';
-// import 'package:crimson_chest_mobile/screens/ItemDetailPage.dart';
-import 'package:http/http.dart' as http;
-import 'package:litera_land_mobile/Discuss/screens/review_detail.dart';
-// import 'package:crimson_chest_mobile/models/product.dart';
-import 'package:litera_land_mobile/Discuss/widget/left_drawer.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:litera_land_mobile/Discuss/models/review_data.dart';
+import 'package:litera_land_mobile/Discuss/screens/review_detail.dart';
+import 'package:litera_land_mobile/Discuss/screens/review_form.dart';
+import 'package:litera_land_mobile/Discuss/widget/left_drawer.dart';
 
 class ReviewsPage extends StatefulWidget {
   final int bookId;
@@ -13,20 +12,16 @@ class ReviewsPage extends StatefulWidget {
 
   @override
   // ignore: library_private_types_in_public_api
-  _ItemsPageState createState() => _ItemsPageState();
+  _ReviewsPageState createState() => _ReviewsPageState();
 }
 
-class _ItemsPageState extends State<ReviewsPage> {
+class _ReviewsPageState extends State<ReviewsPage> {
   Future<List<ReviewBook>> fetchProduct() async {
-    // Update the URL to point to the book_reviews endpoint with the specific bookId
     var url = Uri.parse(
         'https://literaland-c07-tk.pbp.cs.ui.ac.id/forumDiskusi/json_id/${widget.bookId}/');
-    var response = await http.get(
-      url,
-      headers: {"Content-Type": "application/json"},
-    );
+    var response =
+        await http.get(url, headers: {"Content-Type": "application/json"});
 
-    // Check if the response is successful and decode the JSON
     if (response.statusCode == 200) {
       var data = jsonDecode(utf8.decode(response.bodyBytes));
       List<ReviewBook> listProduct = [];
@@ -56,7 +51,7 @@ class _ItemsPageState extends State<ReviewsPage> {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(child: CircularProgressIndicator());
           } else if (snapshot.hasError) {
-            return const Center(child: Text('Error fetching products'));
+            return const Center(child: Text('Error fetching reviews'));
           } else if (snapshot.hasData) {
             return ListView.separated(
               itemCount: snapshot.data!.length,
@@ -86,7 +81,7 @@ class _ItemsPageState extends State<ReviewsPage> {
                             style: Theme.of(context).textTheme.titleLarge,
                           ),
                           const SizedBox(height: 8),
-                          Text("Rating: \$${product.fields.starRating}"),
+                          Text("Rating: ${product.fields.starRating}"),
                           const SizedBox(height: 8),
                           Text("Review: ${product.fields.review}"),
                         ],
@@ -97,9 +92,23 @@ class _ItemsPageState extends State<ReviewsPage> {
               },
             );
           } else {
-            return const Center(child: Text('No products found'));
+            return const Center(child: Text('No reviews found'));
           }
         },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Replace with the actual navigation to your ItemFormPage
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => ItemFormPage(
+                      bookId: widget.bookId,
+                    )),
+          );
+        },
+        tooltip: 'Add Review',
+        child: const Icon(Icons.add),
       ),
     );
   }
